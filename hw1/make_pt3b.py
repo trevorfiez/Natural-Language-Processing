645adf719612452dbce9da96e8269047681f8f9a
trevorfiez <fieztr@onid.oregonstate.edu>
2017-04-17 17:26:02 -0700

    Part 3b

---
+++ hw1/make_pt3b.py
@@ -0,0 +1,36 @@
+import sys
+
+def is_vowel(l):
+
+	if (l == "A" or l == "E" or l == "I" or l == "O" or l == "U"):
+		return True
+	return False
+
+
+def main():
+	sys.stdout.write('F\n')
+
+	max_state = 1
+
+	for line in sys.stdin:
+
+		words = line.split(',')
+		letters = words[0].split()
+		prob = float(words[1])
+
+		sys.stdout.write('(0 (%d *e* *e* %f))\n' % (max_state, prob))
+		max_state += 1
+		for l in letters:
+			if (is_vowel(l)):
+				sys.stdout.write('(%d (%d ' % (max_state - 1, max_state) + l + ' *e*))\n' )
+			else:
+				sys.stdout.write('(%d (%d ' % (max_state - 1, max_state) + l + ' ' + l + '))\n' )
+
+			max_state += 1
+		sys.stdout.write('(%d (%d _ _))\n' % (max_state - 1, 0))
+		sys.stdout.write('(%d (F *e* *e*))\n' % (max_state - 1))
+
+
+
+if __name__ == "__main__":
+	main()
--- hw1/pt3b.py
+++ hw1/pt3b.py
@@ -1,20 +1,33 @@
 import sys
 import os
+import re
 from collections import Counter
 
 def main():
     # change the os environment to wherever the hw data directory is
-    vocab = open('{0}/vocab'.format(os.environ['HW1DATA']), 'r').read().lower().split(' \n')
-    corpus = sys.stdin.read().lower().split()
+    vocab = []
+    f = open(sys.argv[1], 'r')
+    
+    for line in f:
+        vocab.append(line.strip().replace(' ', ''))
 
+   
+    #vocab = sys.stdin.read().lower().split(' \n')
+    corpus = re.findall(r'\w+', sys.stdin.read().upper())
+
+    #print(len(corpus))
     counts = Counter(corpus)
 
     freq = dict(zip(vocab, [1] * len(vocab)))
 
+    total = 0
     for word in vocab:
         if word in counts:
-            freq[word] = counts[word]
-        sys.stdout.write('{0},{1} \n'.format(word.upper(), freq[word]))
-
+            freq[word] += counts[word]
+            total += freq[word]
+    
+    for word in vocab:
+        sys.stdout.write('{0},{1} \n'.format(' '.join(list(word)), freq[word] / float(total)))
+	
 if __name__ == "__main__":
     main()
